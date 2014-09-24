94afb0caf5801541dc316ada04cec930c4de0b67
Daniel Treasure <dbtreasure@gmail.com>
2014-09-23 20:01:43 -0600

    added cors handling

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express();
 var requestify = require('requestify');
 
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 app.get('/kimonolabs', function(req, res){
   requestify.get('https://www.kimonolabs.com/api/9g57gd1i?apikey=nPYNaTq2b8Q9HVz5Wwvu1HqWTDPlvVSL').then(function(response) {
