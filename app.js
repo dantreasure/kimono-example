97f72d86cedddc06d7b5b9dfee9c2607f6d351ca
Daniel Treasure <dbtreasure@gmail.com>
2014-09-23 19:27:19 -0600

    first commit with kimonolabs info

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var requestify = require('requestify');
+
+
+
+app.get('/kimonolabs', function(req, res){
+  requestify.get('https://www.kimonolabs.com/api/9g57gd1i?apikey=nPYNaTq2b8Q9HVz5Wwvu1HqWTDPlvVSL').then(function(response) {
+      res.send(response.getBody());
+  });
+
+
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
